e9892871cef536f475d7e07087c68018ce4d3494
paldeflex <paldeflex@gmail.com>
2025-02-14 21:21:23 +0500

    Добавил языковые файлы

---
+++ lang/vendor/filament-actions/ru/create.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Создать',
+
+        'modal' => [
+
+            'heading' => 'Создать :label',
+
+            'actions' => [
+
+                'create' => [
+                    'label' => 'Создать',
+                ],
+
+                'create_another' => [
+                    'label' => 'Создать и добавить ещё',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'created' => [
+                'title' => 'Создано',
+            ],
+
+        ],
+
+    ],
+
+];
