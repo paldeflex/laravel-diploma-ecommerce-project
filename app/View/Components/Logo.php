2293dcdb6ce21a5c1e9a2efe717136a0143e1e97
paldeflex <paldeflex@gmail.com>
2025-03-03 21:39:22 +0500

    Добавил компонент логотипа

---
+++ app/View/Components/Logo.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Logo extends Component
+{
+    public string $href;
+    public string $iconColor;
+    public string $textColor;
+    public int $iconSize;
+    public string $titleSize;
+    public string $subtitleSize;
+    public string $title;
+    public string $subtitle;
+    public string $icon;
+
+    public function __construct(
+        string $href = '/',
+        string $icon = 'snowflake',
+        string $iconColor = '#fff',
+        int $iconSize = 32,
+        string $textColor = 'text-white',
+        string $titleSize = 'text-sm',
+        string $subtitleSize = 'text-xs',
+        string $title = 'Снежинские краски',
+        string $subtitle = 'Защитные покрытия',
+    ) {
+        $this->href = $href;
+        $this->icon = $icon;
+        $this->iconColor = $iconColor;
+        $this->iconSize = $iconSize;
+        $this->textColor = $textColor;
+        $this->titleSize = $titleSize;
+        $this->subtitleSize = $subtitleSize;
+        $this->title = $title;
+        $this->subtitle = $subtitle;
+    }
+
+    public function render()
+    {
+        return view('components.logo');
+    }
+}
---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,11 @@
+<a href="{{ $href }}" aria-label="{{ $title }}">
+    <span class="logo flex items-center space-x-3 ">
+        <span class="logo__icon flex items-center justify-center">
+            @includeIf("icons.{$icon}", ['iconSize' => $iconSize, 'iconColor' => $iconColor])
+        </span>
+        <span class="logo__text flex flex-col justify-center leading-tight {{ $textColor }}">
+            <p class="font-semibold {{ $titleSize }} whitespace-nowrap">{{ $title }}</p>
+            <small class="{{ $subtitleSize }} text-gray-300 whitespace-nowrap">{{ $subtitle }}</small>
+        </span>
+    </span>
+</a>
