42625ffd1e46b9f6485c01fca20cc4f5c1caa7c9
paldeflex <paldeflex@gmail.com>
2025-03-06 20:27:22 +0500

    Добавил удаление изображений продукции при удалении записи из БД

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Observers\ProductObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[ObservedBy([ProductObserver::class])]
 class Product extends Model
 {
     use HasFactory;
---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Storage;
+
+class ProductObserver
+{
+    /**
+     * Handle the Product "deleted" event.
+     */
+    public function deleted(Product $product): void
+    {
+        if (!empty($product->images) && is_array($product->images)) {
+            foreach ($product->images as $image) {
+                Storage::disk('public')->delete($image);
+            }
+        }
+    }
+}
