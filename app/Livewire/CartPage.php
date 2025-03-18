f95a1bb2b85a9c1e8d74aed1c0ea686bcddaa995
paldeflex <paldeflex@gmail.com>
2025-03-18 18:00:08 +0500

    Логика добавления товаров в корзину

--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -2,10 +2,54 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[title('Корзина - Снежинские краски')]
 class CartPage extends Component
 {
+    public $cartItems = [];
+    public $grandTotal;
+
+    private function addImageUrlToCartItems(): void
+    {
+        foreach ($this->cartItems as &$item) {
+            $item['image_url'] = $item['image']
+                ? url('storage', $item['image'])
+                : asset('images/product-not-found.webp');
+        }
+    }
+
+    public function increaseQty($id): void
+    {
+        $this->cartItems = CartManagement::incrementQuantityToCartItem($id);
+        $this->addImageUrlToCartItems();
+        $this->grandTotal = CartManagement::calculateGrandTotal($this->cartItems);
+    }
+
+    public function decreaseQty($id): void
+    {
+        $this->cartItems = CartManagement::decrementQuantityToCartItem($id);
+        $this->addImageUrlToCartItems();
+        $this->grandTotal = CartManagement::calculateGrandTotal($this->cartItems);
+    }
+
+    public function mount(): void
+    {
+        $this->cartItems = CartManagement::getCartItemsFromCookie();
+        $this->addImageUrlToCartItems();
+        $this->grandTotal = CartManagement::calculateGrandTotal($this->cartItems);
+    }
+
+    public function removeItem($id): void
+    {
+        $this->cartItems = CartManagement::removeItemFromCart($id);
+        $this->addImageUrlToCartItems();
+        $this->grandTotal = CartManagement::calculateGrandTotal($this->cartItems);
+        $this->dispatch('updateCartCount', count($this->cartItems))->to(Navbar::class);
+    }
     public function render()
     {
         return view('livewire.cart-page');
--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -3,7 +3,6 @@
     <div class="flex flex-col lg:flex-row gap-4">
         <div class="w-full lg:w-3/4">
             <div class="bg-white rounded-lg shadow-md p-4 sm:p-6 mb-4">
-                <!-- Оберните таблицу в дополнительный контейнер и установите минимальную ширину для таблицы на маленьких экранах -->
                 <div class="overflow-x-auto">
                     <table class="w-full" style="min-width: 700px;">
                         <thead>
@@ -16,31 +15,41 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td class="py-4 px-2">
-                                <div class="flex items-center">
-                                    <img class="h-16 w-16 mr-4" src="https://picsum.photos/150"
-                                         alt="Изображение товара">
-                                    <span class="font-semibold">Название товара</span>
-                                </div>
-                            </td>
-                            <td class="py-4 px-2">1499 &#8381;</td>
-                            <td class="py-4 px-2">
-                                <div class="flex items-center">
-                                    <button class="border rounded-md py-2 px-4 mr-2">-</button>
-                                    <span class="text-center w-8">1</span>
-                                    <button class="border rounded-md py-2 px-4 ml-2">+</button>
-                                </div>
-                            </td>
-                            <td class="py-4 px-2">1499 &#8381;</td>
-                            <td class="py-4 px-2">
-                                <button
-                                    class="bg-slate-300 border-2 border-slate-400 rounded-lg px-3 py-1 hover:bg-red-500 hover:text-white hover:border-red-700">
-                                    Удалить
-                                </button>
-                            </td>
-                        </tr>
-                        <!-- Дополнительные строки товаров -->
+                        @forelse($cartItems as $cartItem)
+                            <tr wire:key="{{ $cartItem['id'] }}">
+                                <td class="py-4 px-2">
+                                    <div class="flex items-center">
+                                        <img class="h-16 w-16 mr-4" src="{{ $cartItem['image_url'] }}"
+                                             alt="{{$cartItem['name']}}">
+                                        <span class="font-semibold">{{$cartItem['name']}}</span>
+                                    </div>
+                                </td>
+                                <td class="py-4 px-2">{{ Number::currency($cartItem['unitAmount'], 'RUB', 'ru_RU') }}</td>
+                                <td class="py-4 px-2">
+                                    <div class="flex items-center">
+                                        <button wire:click="decreaseQty({{$cartItem['id']}})" class="border rounded-md py-2 px-4 mr-2">-</button>
+                                        <span class="text-center w-8">{{$cartItem['quantity']}}</span>
+                                        <button wire:click="increaseQty({{$cartItem['id']}})" class="border rounded-md py-2 px-4 ml-2">+</button>
+                                    </div>
+                                </td>
+                                <td class="py-4 px-2">{{ Number::currency($cartItem['totalAmount'], 'RUB', 'ru_RU') }}</td>
+                                <td class="py-4 px-2">
+                                    <button
+                                        wire:click="removeItem({{ $cartItem['id'] }})"
+                                        class="bg-slate-300 border-2 border-slate-400 rounded-lg px-3 py-1 hover:bg-red-500 hover:text-white hover:border-red-700">
+                                        <span wire:loading.remove wire:target="removeItem({{ $cartItem['id'] }})">Удалить</span>
+                                        <span wire:loading wire:target="removeItem({{ $cartItem['id'] }})">Удаление...</span>
+                                    </button>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="5" class="text-center py-4 text-4xl font-semibold">
+                                    <p>Корзина пуста</p>
+                                </td>
+                            </tr>
+
+                        @endforelse
                         </tbody>
                     </table>
                 </div>
@@ -51,22 +60,24 @@
                 <h2 class="text-lg font-semibold mb-4">Сводка</h2>
                 <div class="flex justify-between mb-2">
                     <span>Подытог</span>
-                    <span>1499 &#8381;</span>
+                    <span>{{ Number::currency($grandTotal, 'RUB', 'ru_RU') }}</span>
                 </div>
                 <div class="flex justify-between mb-2">
                     <span>Налоги</span>
-                    <span>149 &#8381;</span>
+                    <span>{{ Number::currency(0, 'RUB', 'ru_RU') }}</span>
                 </div>
                 <div class="flex justify-between mb-2">
                     <span>Доставка</span>
-                    <span>0 &#8381;</span>
+                    <span>{{ Number::currency(0, 'RUB', 'ru_RU') }}</span>
                 </div>
                 <hr class="my-2">
                 <div class="flex justify-between mb-2">
                     <span class="font-semibold">Итого</span>
-                    <span class="font-semibold">1648 &#8381;</span>
+                    <span class="font-semibold">{{ Number::currency(0, 'RUB', 'ru_RU') }}</span>
                 </div>
-                <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Оформить заказ</button>
+                @if($cartItems)
+                    <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Оформить заказ</button>
+                @endif
             </div>
         </div>
     </div>
